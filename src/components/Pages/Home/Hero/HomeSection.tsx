e161c3ea5995b8e6a926e02dd12626aa70b3e498
Matheus Silva <matheussilva@Matheuss-MacBook-Air.local>
2024-04-22 13:09:14 -0300

    first commit

--- src/components/Pages/Home/Hero/HomeSection.tsx
+++ src/components/Pages/Home/Hero/HomeSection.tsx
@@ -1,11 +1,12 @@
 import ShortcutHome from "@/components/ui/shortCutHome.tsx";
 import HeroAnimationWrapper from "./HeroAnimationWrapper";
 import HeroText from "./HeroText";
+import TechStackAnimation from "./TechStackAnimation";
 
 export default function HeroSection() {
   return (
     <section className="relative flex h-screen flex-col items-center justify-center py-20 sm:gap-5">
-      {/* <TechStackAnimation /> */}
+      <TechStackAnimation />
       <HeroAnimationWrapper>
         <HeroText />
         <ShortcutHome />
